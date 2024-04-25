c0c4418c7ecc39426d0dccbbeae0937854e32b2d
Dilnaz <230719@astanait.edu.kz>
2024-04-25 20:56:53 +0500

    12 commit

---
+++ src/MyStack.java
@@ -0,0 +1,40 @@
+import java.util.NoSuchElementException;
+public class MyStack {
+    public class CustomStack<T extends Comparable<T>> {
+        private MyDoublyLinkedList<T> customList = new MyDoublyLinkedList<T>();
+
+        // Check if the stack is empty
+        public boolean isEmpty() {
+            return customList.size() == 0;
+        }
+
+        // Peek at the top element of the stack without removing it
+        public T peek() {
+            if (isEmpty()) {
+                throw new NoSuchElementException("Stack is empty");
+            }
+            return customList.getLast();
+        }
+
+        // Get the size of the stack
+        public int getSize() {
+            return customList.size();
+        }
+
+        // Push an element onto the stack
+        public void push(T element) {
+            customList.addLast(element);  // Elements are added to the end for LIFO
+        }
+
+        // Pop the top element from the stack
+        public T pop() {
+            if (isEmpty()) {
+                throw new NoSuchElementException("Stack is empty");
+            }
+            T element = customList.getLast();  // Get the last element
+            customList.removeLast();           // Remove the last element
+            return element;
+        }
+    }
+
+}
